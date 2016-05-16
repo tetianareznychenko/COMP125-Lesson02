29a2954ea7de19c06f18bfdce00a3a30ee6029e4
tetianareznychenko <tatiana.reznuchenko@gmail.com>
2016-05-16 09:44:45 -0400

    Initial Commit

---
+++ Scripts/app.js
@@ -0,0 +1,12 @@
+/* main JavaScript file */
+
+//IIFE - Immediately Invoked Function Expression
+(function(){
+    "use strict";
+    
+    
+//code goes here  
+
+console.log("App Started...") 
+    
+})();
